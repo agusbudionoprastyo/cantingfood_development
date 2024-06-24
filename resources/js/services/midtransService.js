ce85682332906ed6ac84f912fdcfe946a28e982c
agusbudionoprastyo <agna4605@gmail.com>
2024-06-24 15:01:09 +0700

    a

--- resources/js/services/midtransService.js
+++ resources/js/services/midtransService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const midtransService = {
     processPayment(form) {
-        return axios.post('/api/midtrans/charge', form);
+        return axios.post('/midtrans/charge', form);
     }
 };
 
