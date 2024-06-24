f98980d8a673fbe5a13de32ece6a33f16b290b9b
agusbudionoprastyo <agna4605@gmail.com>
2024-06-24 14:35:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const midtransRoutes = require('./routes/midtrans'); // Import rute Midtrans
 const app = express();
 
 app.use(bodyParser.json()); // Middleware untuk parsing JSON
-app.use('/api/midtrans', midtransRoutes); // Gunakan rute Midtrans
+app.use('/midtrans', midtransRoutes); // Gunakan rute Midtrans
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
